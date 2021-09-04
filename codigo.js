843e485fffda63158c52afce60f02963ca14e9ee
Johan Forero <johanfore67@gmail.com>
2021-09-03 23:10:50 -0500

    Operadores de Comparacion y concatenacion

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
-let numero = 29; //Declaracion de variables var y const
-let string = "pedro";
+// let numero = 29; //Declaracion de variables var y const
+// let string = "pedro";
 
 // alert(numero * nombre); // Ejemplo de NaN
 
@@ -7,6 +7,36 @@ let string = "pedro";
 
 // alert(nombre)
 
-numero += 50;
+// numero += 50;
 
-document.write(numero) // Un tipo de alert
+// document.write(numero) // Un tipo de alert
+
+
+// saludo = "Hola";
+// pregunta = "Johan";
+
+// concatenacion = saludo + pregunta;
+
+// document.write(concatenacion); // Ejemplo de concatenacion con strings
+
+// numero1 = "1";
+// numero2 = 2;
+
+// frase = "" + numero1 + numero2; // COncatencacion forzada para que no se sumen los int
+
+// frase = numero1.concat(numero2) // Para que el concat funcione se debe tener al menos un string
+
+// document.write(frase);
+
+
+// nombre = "Johan Forero";
+
+// otro = `Soy ${nombre} y estoy haciendo el curso de Javascript`; //Uso de backticks
+
+// document.write(otro);
+
+let numero = 23;
+let numero1 = 13;
+
+// document.write(numero == numero1) // Equality
+document.write(numero != numero1); // Inequality
