1ce4f4111c62de2f38871a80a3e901cfae7e479a
Johan Forero <johanfore67@gmail.com>
2021-09-03 23:38:03 -0500

    Condicionales

--- codigo.js
+++ codigo.js
@@ -11,32 +11,45 @@
 
 // document.write(numero) // Un tipo de alert
 
+//COocatenacion
 
 // saludo = "Hola";
 // pregunta = "Johan";
-
 // concatenacion = saludo + pregunta;
 
 // document.write(concatenacion); // Ejemplo de concatenacion con strings
 
 // numero1 = "1";
 // numero2 = 2;
-
 // frase = "" + numero1 + numero2; // COncatencacion forzada para que no se sumen los int
-
 // frase = numero1.concat(numero2) // Para que el concat funcione se debe tener al menos un string
-
 // document.write(frase);
 
 
 // nombre = "Johan Forero";
-
 // otro = `Soy ${nombre} y estoy haciendo el curso de Javascript`; //Uso de backticks
-
 // document.write(otro);
 
-let numero = 23;
-let numero1 = 13;
+//Operadores de comparacion
 
-// document.write(numero == numero1) // Equality
-document.write(numero != numero1); // Inequality
+// let numero = 23;
+// let numero1 = 13;
+
+// document.write(numero == numero1) // Equality (Que sean igual)
+// document.write(numero != numero1); // Inequality (Funciona con textos)
+// document.write(numero === numero1); // Identity (Revisa que los datos sean estrictamente igual incluyendo su tipo de dato)
+// document.write(numero !== numero1); // Non - Identity
+
+//Condicionales
+
+// let numero = 10;
+
+// if (numero == 15) {
+//   alert("Hola");
+// }
+// else if (numero < 10){
+//   alert("Hola Johan");
+// }
+// else{
+//   alert("Hola Desconocido");
+// }
