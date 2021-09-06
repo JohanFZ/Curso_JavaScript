e73958d44ac6f84c811dfc578a7656533f54e2e4
Johan Forero <johanfore67@gmail.com>
2021-09-05 23:12:22 -0500

    Funciones

--- capitulo2.js
+++ capitulo2.js
@@ -82,3 +82,52 @@ let numero = 0;
 
 //Funciones
 
+// function saludar(){
+//   respuesta = prompt("Hola, como te fue el dia de hoy?")
+//   if(respuesta == "bien"){
+//     alert("Me alegro");
+//   }else{
+//     alert("Que pena");
+//   }
+// }
+
+// saludar();
+
+// saludar = function () {
+//   respuesta = prompt("Hola, como te fue el dia de hoy?")
+//   if (respuesta == "bien") {
+//     alert("Me alegro");
+//   } else {
+//     alert("Que pena");
+//   }
+// }
+
+// function saludar (){
+//     alert("Hola");
+//     return "Si sirve";
+// }
+
+// let saludo = saludar();
+
+// document.write(saludo);
+
+function suma (num1 , num2){ // Funcion con parametros
+  let res = num1 + num2;
+  return res;
+}
+
+let resultado = suma(1,58);
+
+// document.write(resultado);
+
+// const saludar = function suma(num1, num2) { // Funcion con parametros
+//   let res = num1 + num2;
+//   return res;
+// }
+
+const saludar = (nombre)=>{
+  let frase = `Hola, ${nombre}, como estas?`;
+  document.write(frase);
+}
+
+saludar("Johan");
