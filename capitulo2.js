975142334011ff39baadc67f6067bb99fcc041ef
Johan Forero <johanfore67@gmail.com>
2021-09-05 22:36:49 -0500

    Bucles e Iteraciones

--- capitulo2.js
+++ capitulo2.js
@@ -27,3 +27,58 @@ frase = `El nombre de mi PC es <b>${nombre}</b> <br>
 
 //Bucles e Iteracion
 
+let numero = 0;
+
+
+// while(numero < 10){
+//   numero += 1;
+//   document.write(numero + "<br>");
+// }
+
+
+// do {
+//     numero += 1;
+//     document.write(numero + "<br>");
+
+// } while (numero < 10);
+
+// for (let i = 0; i < 15; i++) {
+
+//   if (i == 10) {
+//     continue; //Salta el ciclo cuando esta condicion se cumpla
+//   }
+//   if (i == 14) {
+//     break; //deja de realizar algun tipo de bucle
+//   }
+//   document.write(i + "<br>");
+// }
+
+// let animales = ["Gato", "Perro", "Tiranosaurio", "Conejo"];
+
+// for(animal in animales) { //in devuelve la posicion de cada elemento (indice)
+//   document.write(animal + "<br>");
+//   document.write(animales[animal] + "<br>"); //bucle animales elemento animal
+
+// }
+
+// for (animal of animales) { //of devuleve el elemento
+//   document.write(animal + "<br>");
+// }
+
+// let array0 = ["Johan", "Alison", "Isa"];
+// let array1 = ["Claudia", "Santi", array0];
+
+// forEjemplo: //label
+// for(let array in array1){ // Se recorre el segundo array
+//     if (array == 2) {
+//       for(let array of array0){ // Se recorre el array de la posicion 2
+//         document.write(array + "<br>");
+//         break forEjemplo;
+//       }
+//     }else{
+//       document.write(array1[array] + "<br>");
+//     }
+// }
+
+//Funciones
+
