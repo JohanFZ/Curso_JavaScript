5f0c3b6ae36072b3dc89708ba859f36fa8ce9a33
Johan Forero <johanfore67@gmail.com>
2021-09-05 21:37:24 -0500

    Arrays

--- capitulo1.js
+++ capitulo1.js
@@ -1,116 +1,55 @@
-//Historia de Cofla 1
+// let numero = 29; //Declaracion de variables var y const
+// let string = "pedro";
 
-//Metodo Eficiente
+// alert(numero * nombre); // Ejemplo de NaN
 
-const definircompra = (n)=>{
-  let din = prompt(`Dinero de ${n}`);
-  if (din >= 0.6 && din < 1) return (`${n}: helado de agua`);
-  if (din >= 1 && din < 1.6) return (`${n}: helado de crema`);
-  if (din >= 1.6 && din < 1.7) return (`${n}: helado de heladix`);
-  if (din >= 1.7 && din < 1.8) return (`${n}: helado de heladovich`);
-  if (din >= 1.8 && din < 2.9) return (`${n}: helado de helardo`);
-  if (din >= 2.9) return (`${n}: helado de confites o pote de 1/4 kg`);
-  else return (`${n}: No te alcanza para nada`);
-}
+// let nombre = prompt("¿Cual es tu nombre?");
 
-console.log(definircompra("Cofla"));
-console.log(definircompra("Pedro"));
-console.log(definircompra("Roberto"));
+// alert(nombre)
+
+// numero += 50;
+
+// document.write(numero) // Un tipo de alert
+
+//COocatenacion
+
+// saludo = "Hola";
+// pregunta = "Johan";
+// concatenacion = saludo + pregunta;
+
+// document.write(concatenacion); // Ejemplo de concatenacion con strings
+
+// numero1 = "1";
+// numero2 = 2;
+// frase = "" + numero1 + numero2; // COncatencacion forzada para que no se sumen los int
+// frase = numero1.concat(numero2) // Para que el concat funcione se debe tener al menos un string
+// document.write(frase);
 
 
-//Metodo largo
+// nombre = "Johan Forero";
+// otro = `Soy ${nombre} y estoy haciendo el curso de Javascript`; //Uso de backticks
+// document.write(otro);
 
-// let dineroCofla = prompt("Cuanto dinero tienes Cofla?");
-// let montoRoberto = prompt("Cuanto dinero tienes Roberto?");
-// let montopedro = prompt("Cuanto dinero tienes Pedro?");
+//Operadores de comparacion
 
-// dineroCofla = parseInt(dineroCofla);
-// montoRoberto = parseInt(montoRoberto);
-// montopedro = parseInt(montopedro);
+// let numero = 23;
+// let numero1 = 13;
 
-// //Condiciones Cofla
+// document.write(numero == numero1) // Equality (Que sean igual)
+// document.write(numero != numero1); // Inequality (Funciona con textos)
+// document.write(numero === numero1); // Identity (Revisa que los datos sean estrictamente igual incluyendo su tipo de dato)
+// document.write(numero !== numero1); // Non - Identity
 
-// if(dineroCofla > 0.6 && dineroCofla < 1){
-//   alert("Puedes comprar un Palito de gelado de agua");
-//   alert("Tus vueltas son : " + (dineroCofla - 0.6));
-// }
-// else if(dineroCofla > 0.99 && dineroCofla < 1.6){
-//   alert("Puedes comprar un Palito de crema");
-//   alert("Tus vueltas son : " + (dineroCofla - 1));
-// }
-// else if (dineroCofla > 1.59 && dineroCofla < 1.7) {
-//   alert("Puedes comprar un Bombom de helado marca heladix");
-//   alert("Tus vueltas son : " + (dineroCofla - 1.6));
-// }
-// else if (dineroCofla > 1.69 && dineroCofla < 1.8) {
-//   alert("Puedes comprar un Bombom de helado marca heladovich");
-//   alert("Tus vueltas son : " + (dineroCofla - 1.7));
-// }
-// else if (dineroCofla > 1.79 && dineroCofla < 2.9) {
-//   alert("Puedes comprar un Bombom de helado marca helardo");
-//   alert("Tus vueltas son : " + (dineroCofla - 1.8));
-// }
-// else if(dineroCofla >2.89){
-//   alert("Puedes comparar un Potecito de helado con conftes o un potye de 1/4 KG");
-//   alert("Tus vueltas son : " + (dineroCofla - 2.9));
-// }else{
-//   alert("No te alcanza para nada");
-// }
+//Condicionales
 
-// //Condiciones Roberto
+// let numero = 10;
 
-// if (montoRoberto > 0.6 && montoRoberto < 1) {
-//   alert("Puedes comprar un Palito de gelado de agua");
-//   alert("Tus vueltas son : " + (montoRoberto - 0.6));
+// if (numero == 15) {
+//   alert("Hola");
 // }
-// else if (montoRoberto > 0.99 && montoRoberto < 1.6) {
-//   alert("Puedes comprar un Palito de crema");
-//   alert("Tus vueltas son : " + (montoRoberto - 1));
+// else if (numero < 10){
+//   alert("Hola Johan");
 // }
-// else if (montoRoberto > 1.59 && montoRoberto < 1.69) {
-//   alert("Puedes comprar un Bombom de helado marca heladix");
-//   alert("Tus vueltas son : " + (montoRoberto - 1.6));
-// }
-// else if (montoRoberto > 1.69 && montoRoberto < 1.79) {
-//   alert("Puedes comprar un Bombom de helado marca heladovich");
-//   alert("Tus vueltas son : " + (montoRoberto - 1.7));
-// }
-// else if (montoRoberto > 1.79 && montoRoberto < 2.89) {
-//   alert("Puedes comprar un Bombom de helado marca helardo");
-//   alert("Tus vueltas son : " + (montoRoberto - 1.8));
-// }
-// else if (montoRoberto > 2.89) {
-//   alert("Puedes comparar un Potecito de helado con conftes o un potye de 1/4 KG");
-//   alert("Tus vueltas son : " + (montoRoberto - 2.9));
-// } else {
-//   alert("No te alcanza para nada");
-// }
-
-// //Condiciones Pedro
-
-// if (montopedro > 0.6 && montopedro < 1) {
-//   alert("Puedes comprar un Palito de gelado de agua");
-//   alert("Tus vueltas son : " + (montopedro - 0.6));
-// }
-// else if (montopedro > 0.99 && montopedro < 1.6) {
-//   alert("Puedes comprar un Palito de crema");
-//   alert("Tus vueltas son : " + (montopedro - 1));
-// }
-// else if (montopedro > 1.59 && montopedro < 1.69) {
-//   alert("Puedes comprar un Bombom de helado marca heladix");
-//   alert("Tus vueltas son : " + (montopedro - 1.6));
-// }
-// else if (montopedro > 1.69 && montopedro < 1.79) {
-//   alert("Puedes comprar un Bombom de helado marca heladovich");
-//   alert("Tus vueltas son : " + (montopedro - 1.7));
-// }
-// else if (montopedro > 1.79 && montopedro < 2.89) {
-//   alert("Puedes comprar un Bombom de helado marca helardo");
-//   alert("Tus vueltas son : " + (montopedro - 1.8));
-// }
-// else if (montopedro > 2.89) {
-//   alert("Puedes comparar un Potecito de helado con conftes o un potye de 1/4 KG");
-//   alert("Tus vueltas son : " + (montopedro - 2.9));
-// } else {
-//   alert("No te alcanza para nada");
+// else{
+//   alert("Hola Desconocido");
 // }
---
+++ capitulo2.js
@@ -0,0 +1,29 @@
+//arrays
+
+let frutas = ["Banana", "Uva", "Fresa", "Pera"];
+
+// document.write(frutas[0]);
+
+//Arrays Asociativos (Objetos)
+
+let pc = {
+  nombre: "JohanPC",
+  procesador: "Intel Core I7",
+  ram: "16",
+  espacio: "1TB"
+}
+
+// document.write(pc["nombre"]);
+
+let nombre = pc["nombre"];
+let ram = pc["ram"];
+
+
+frase = `El nombre de mi PC es <b>${nombre}</b> <br>
+        la memoria ram es de <b>${ram} Gb</b>`;
+
+// document.write(frase);
+
+
+//Bucles e Iteracion
+
