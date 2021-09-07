c3d7fc4c58a0992fb38232ebc0a51e205996f00b
Johan Forero <johanfore67@gmail.com>
2021-09-06 21:10:43 -0500

    Metodos de cadenas

---
+++ capitulo4.js
@@ -0,0 +1,28 @@
+//Metodos de Cadena
+
+let cadena = "   Hola soy Johan   ";
+let cadena2 = "de";
+
+// resultado = cadena.concat(cadena2); // concatena dos cadenas de texto
+// let resultado = cadena.startsWith(cadena2);
+// let resultado = cadena.endsWith(cadena2);
+// let resultado = cadena.includes(cadena2);
+// let resultado = cadena.indexOf("de");
+// let resultado = cadena.lastIndexOf("de");
+// let resultado = cadena.padStart(6, "a");
+// let resultado = cadena.padEnd(6, "a");
+// let resultado = cadena.repeat(2);
+// let resultado = cadena.split(" ");
+// let resultado = cadena.substring(0,4);
+// let resultado = cadena.toLowerCase();
+// let resultado = cadena.toString();
+// let resultado = cadena.trimStart();
+
+
+// document.write(resultado);
+// console.log(resultado)
+
+
+//Metodos de Arrays
+
+
