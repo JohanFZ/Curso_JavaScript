2c73256ee34d19c644172700f66c773587859fb5
Johan Forero <johanfore67@gmail.com>
2021-09-20 19:54:48 -0500

    Funciones de registro (consola)

--- capitulo5.js
+++ capitulo5.js
@@ -1,1 +1,14 @@
 //Consola
+
+//Funciones de registro
+
+console.assert(5 < 3); // Genera un mensaje si una condicion es falsa
+console.clear(); // Limpia consola
+console.error("Este es un error"); // Muestra mensaje con estilo a error
+console.info("Este es un mensaje informativo"); // Mensaje informativo
+console.log("Este es un mensaje de depuración");// Mensaje de depuracion
+console.table([4, 2, 4, 5, 8, 9, 1, 4, 3, 77, 4]);// Muestra datos a traves de una table, recibe un array o un objeto
+console.warn("Esto es una advertencia"); // Mensaje de Advertencia
+console.dir([4, 2, 4, 5, 8, 9, 1, 4, 3, 77, 4]); //Despliega una lista de elementos de un array
+
+//Funciones de Conteo
